b9af7a3e149ca6a929445f5f9fc5201551fb1517
Avijeet341 <80438115+Avijeet341@users.noreply.github.com>
2023-08-14 13:30:13 +0530

    Update MainActivity.kt

--- app/src/main/java/com/avi/funquizgame/MainActivity.kt
+++ app/src/main/java/com/avi/funquizgame/MainActivity.kt
@@ -54,10 +54,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onBackPressed() {
-        val intent = Intent(this, MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-        startActivity(intent)
-        finish()
-    }
+    
 }
